07cef5ed3dc4a4630d3ed482b780513cefe6aa8b
kavijarr <kavijakumuditha12@gmail.com>
2024-02-26 21:54:16 +0530

    User registration form

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -10,10 +11,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit{
+
+  
   
   private http;
   public countryList:any;
   public selectedCountry:any;
+  public isExistUser:any;
+  public userObj={
+    firstName:null,
+    lastName:null,
+    userName:null,
+    email:null,
+    address:null,
+    address2:null,
+    country:null,
+    phoneNumber:null
+  }
 
   constructor(private httpClient:HttpClient){
     this.http=httpClient;
@@ -34,5 +48,34 @@ export class RegisterComponent implements OnInit{
 
   setSelectedCountry(country:any){
     this.selectedCountry=country;
+  }
+
+  submitForm(){
+    console.log(this.userObj);
+    this.http.get(`http://localhost:8081/user/exist-by-userName/${this.userObj.userName}`).subscribe(data=>{
+      console.log(data);
+      this.isExistUser=data;
+      this.registerUser(this.isExistUser);
+    })
+  }
+
+  registerUser(isExistUser:any){
+    if(!isExistUser){
+      this.http.post("http://localhost:8081/user/add",this.userObj).subscribe(data=>{
+        Swal.fire({
+          title:"Success",
+          text:`${this.userObj.userName} Has been registerd !`,
+          icon:"success"
+        })
+      })
+    }else{
+      Swal.fire({
+        title:"Cant register this user",
+        text:`${this.userObj.userName} Has already been registerd !`,
+        icon:"error"
+      })
+    }
+    console.log(isExistUser);
+    
   }
 }
